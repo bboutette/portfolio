ef33d7242c56a1702d1fecb149ad951dfd995aa6
Mark McDermott <mcdermottsolutions@gmail.com>
2017-04-22 01:15:21 -0500

    swapping in real portfolio, removing placeholder site

---
+++ main.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('#myCarousel').carousel();
+    $(".fadeIn").css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 5000);
+});
+
+
+
+
+
+    //fade in with scroll??
+    //Show/Hide Text on email confirmation in form
+    //Store email addresses of subscribers
+    //
+
+    //})
+    // $(window).scroll( function(){
+    //
+    //     /* Check the location of each desired element */
+    //     $('container').each( function(i){
+    //
+    //         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+    //         var bottom_of_window = $(window).scrollTop() + $(window).height();
+    //
+    //         /* If the object is completely visible in the window, fade it in */
+    //         if( bottom_of_window > bottom_of_object ){
+    //
+    //             $(this).animate({'opacity':'1'},500);
+    //
+    //         }
+    //
+    //     });
