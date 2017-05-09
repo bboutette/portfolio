64a9e1e8ad8db6f46af9046fce47adab21d419df
bboutette <bboutette@gmail.com>
2017-05-08 23:16:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $('#myCarousel').carousel();
-//     $(".fadeIn").css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 5000);
-    
+    // $(".fadeIn").css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 5000);
     $(window).scroll( function(){
-    
+
         /* Check the location of each desired element */
         $('.container').each( function(i){
-    
+
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-    
+
             /* If the object is completely visible in the window, fade it in */
             if( bottom_of_window > bottom_of_object ){
-    
-                $(this).animate({'opacity':'1'},3000);
-    
+
+                $(this).animate({'opacity':'1'},500);
+
             }
-    
+
         });
-});
+
+    });
+
 
 
 
