edc281f96591fb668e1a167acfaf1c38958476d2
b b <bboutette@gmail.com>
2017-06-07 02:42:34 -0500

    changed slideshow to a different gallery

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    $("#slides").slidesjs({
-        width: 940,
-        height: 528
-});
-
 
     // Carousel Begins
-    $('#myCarousel').carousel();
+    $('#myCarousel').carousel(500);
     // fade in on dom hit
     $(".fadeIn").css({opacity: 0, visibility: "visible"}).animate({opacity: 1.0}, 1000);
     // fade in on scroll to top of container
     $(window).scroll( function(){
       // Carousel Ends
-      // Slider begins
-      // $('.bxslider').bxSlider();
-      // Slider Ends
+
       // FadeIn Begins
         /* Check the location of each desired element */
         $('.container').each( function(i){
@@ -34,20 +27,6 @@ $( document ).ready(function() {
 
     });
     // FadeIn Ends
-
-    // LightBox SlideShow Begins
-
-  $(".slideshow-img:gt(0)").hide();
-
-  setInterval(function() {
-  $('.slideshow-img:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-    },  3000);
-
 
 
 });
